c897e8eadbe85bb14cc7197c808b5e9a2b8b7bbb
Irsyad Danisaputra <irsyad.sp21@gmail.com>
2024-04-28 20:29:38 +0700

    JS 10 - Prak 2

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -5,18 +5,26 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Foundation\Auth\User;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Foundation\Auth\User as Authenticable;
 
-class UserModel extends User
+class UserModel extends Authenticable implements JWTSubject
 {
-    use HasFactory;
+    // use HasFactory;
+
+    public function getJWTIdentifier() {
+        return $this->getKey();
+    }
+    public function getJWTCustomClaims() {
+        return [];
+    }
 
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
 
     // protected $fillable = ['user_id', 'level_id', 'username', 'nama', 'password'];
-    protected $fillable = ['level_id', 'username', 'nama', 'password'];
     // protected $fillable = ['level_id', 'username', 'nama'];
+    protected $fillable = ['level_id', 'username', 'nama', 'password'];
 
     public function level(): BelongsTo
     {
