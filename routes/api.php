a8dd0d347d4fa28a69d021dfbc80dd05f05d273d
Irsyad Danisaputra <irsyad.sp21@gmail.com>
2024-04-29 12:03:22 +0700

    JS 10 - Prak 4

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\RegisterController;
+use App\Http\Controllers\Api\LevelController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -25,4 +26,10 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout');
+Route::post('/logout', App\Http\Controllers\Api\LogoutController::class)->name('logout ');
+
+Route::get('levels', [LevelController::class, 'index']);               
+Route::post('levels', [LevelController::class, 'store']);            
+Route::get('levels/{level}', [LevelController::class, 'show']);          
+Route::put('levels/{level}', [LevelController::class, 'update']);        
+Route::delete('levels/{level}', [LevelController::class, 'destroy']);    
